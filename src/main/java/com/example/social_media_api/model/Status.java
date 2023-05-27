34ef4afbcc7307f10760b7a93c61dda78c53dad1
Ruslan Satybaev <medtehnik850@gmail.com>
2023-05-27 19:36:27 +0700

    Initial commit

---
+++ src/main/java/com/example/social_media_api/model/Status.java
@@ -0,0 +1,7 @@
+package com.example.social_media_api.model;
+
+public enum Status {
+    SUCCESS,
+    USER_ALREADY_EXISTS,
+    FAILURE
+}
---
+++ src/main/java/com/example/social_media_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.social_media_api.repository;
+
+
+import com.example.social_media_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
